5483f9bc3fbcb5d9196923e201dea2037c80b32a
MGrabler <GAB19925@spengergasse.at>
2021-04-02 22:08:08 +0200

    MGrabler: new Class Customer with firstName and customerID also getter/setter

---
+++ src/model/Customer.java
@@ -0,0 +1,69 @@
+package model;
+
+public class Customer
+{	
+	//----------------------------------Attribute----------------------------------------------------------------------------------
+	private int customerID; 
+	private String firstName;
+
+	//----------------------------------Constructor--------------------------------------------------------------------------------	
+	public Customer(int customerID, String firstName)
+	{
+		setCustomerID(customerID);
+		setFirstName(firstName);
+	}
+
+	//----------------------------------Getter-------------------------------------------------------------------------------------
+	public int getCustomerID()
+	{
+		return customerID;
+	}
+
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	
+	//----------------------------------Setter-------------------------------------------------------------------------------------
+	
+	public void setCustomerID(int customerID)
+	{
+		if(customerID >= 0)
+			if(customerID <= 100_000)
+				this.customerID = customerID;
+			else
+				System.out.println("Fehler bei setPrice("+ customerID +"): Die customerID darf nicht größer 100.000 sein!!!");
+		else
+			System.out.println("Fehler bei setPrice("+ customerID +"): Der Die customerID darf nicht kleiner 0 sein!!!"); 
+	}		
+	public void setFirstName(String firstName)
+	{
+		if(firstName != null)
+			if( ! firstName.isEmpty())
+				if(firstName.length() <= 100)
+					this.firstName = firstName.trim();
+				else
+					System.out.println("Fehler bei setName("+ firstName.length() +"): Es sind Maximal 100 Zeichen erlaubt!!!"); 
+			else
+				System.out.println("Fehler bei setName(...): Der Name ist leer!!!"); 
+		else
+			System.out.println("Fehler bei setName(...): Null-Ref. erhalten!!!"); 
+	}
+	//----------------------------------others-------------------------------------------------------------------------------------
+
+	//----------------------------------Filesystem---------------------------------------------------------------------------------
+
+	//----------------------------------toString-----------------------------------------------------------------------------------
+	@Override
+	public String toString()
+	{
+		StringBuilder builder = new StringBuilder();
+		builder.append("Customer [customerID=");
+		builder.append(customerID);
+		builder.append(", firstName=");
+		builder.append(firstName);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
